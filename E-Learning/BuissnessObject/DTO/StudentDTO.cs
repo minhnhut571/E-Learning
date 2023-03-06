e0d252020a6916e507f46524e01f4b232ae9844b
nghia123vn <123vn2000@gmail.com>
2023-03-06 21:35:35 +0700

    Admin Login

---
+++ E-Learning/BuissnessObject/DTO/StudentDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BuissnessObject.DTO
+{
+	public class StudentDTO
+	{
+		public string StudentId { get; set; }
+		public string StudentName { get; set; }
+		public string Email { get; set; }
+		public string Password { get; set; }
+		public string Address { get; set; }
+		public string Phone { get; set; }
+		public DateTime DateOfBirth { get; set; }
+		public bool Status { get; set; }
+		public DateTime CreateDate { get; set; }
+	}
+}
--- E-Learning/E-LearningAPI/Startup.cs
+++ E-Learning/E-LearningAPI/Startup.cs
@@ -1,59 +1,58 @@
+using BuissnessObject;
+using BuissnessObject.Repository.AdminRepository;
+using DataAccess.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace E_LearningAPI
 {
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 
-        public IConfiguration Configuration { get; }
+		public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddDbContext<ECourseDBContext>();
+			services.AddControllers();
+			services.AddSwaggerGen(c =>
+			{
+				c.SwaggerDoc("v1", new OpenApiInfo { Title = "E_LearningAPI", Version = "v1" });
+			});
 
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "E_LearningAPI", Version = "v1" });
-            });
-        }
+			services.AddScoped<AdminDAO>();
+			services.AddScoped<IAdminRepository, AdminRepository>();
+		}
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "E_LearningAPI v1"));
-            }
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+				app.UseSwagger();
+				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "E_LearningAPI v1"));
+			}
 
-            app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
-    }
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllers();
+			});
+		}
+	}
 }
